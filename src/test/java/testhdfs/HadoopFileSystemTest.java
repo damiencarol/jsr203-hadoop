ab5d46e24a7d82f49133d5da863216a0d7282107
Damien CAROL <damien.carol@gmail.com>
2014-03-20 11:44:41 +0100

    Add unit tests for : Files.createDirectory(path) Files.createFile(path) Files.getLastModifiedTime(path)

--- src/test/java/testhdfs/HadoopFileSystemTest.java
+++ src/test/java/testhdfs/HadoopFileSystemTest.java
@@ -14,7 +14,7 @@ import java.nio.file.spi.FileSystemProvider;
 import junit.framework.TestCase;
 
 public class HadoopFileSystemTest extends TestCase {
-	
+
 	private static final int port = 8020;
 	public static String host = "nc-h04";
 
@@ -22,7 +22,7 @@ public class HadoopFileSystemTest extends TestCase {
 	 * Check that a FileSystemProvider handle 'hdfs' scheme.
 	 */
 	public void testAutoRegister() {
-		
+
 		boolean found = false;
 		for (FileSystemProvider fp : FileSystemProvider.installedProviders())
 			if (fp.getScheme().equals(HadoopFileSystemProvider.SCHEME))
@@ -46,10 +46,28 @@ public class HadoopFileSystemTest extends TestCase {
 		Files.delete(path);
 		assertFalse(Files.exists(path));
 	}
-	
+
 	public void testDefaults() throws URISyntaxException, IOException {
 		URI uri = new URI("hdfs://" + host + ":" + port + "/tmp/toto");
 		Path path = Paths.get(uri);
 		Files.deleteIfExists(path);
+	}
+
+	public void testLastModifiedTime() throws URISyntaxException, IOException {
+		URI uri = new URI("hdfs://" + host + ":" + port + "/tmp/toto");
+		Path path = Paths.get(uri);
+		Files.createDirectory(path);
+		assertTrue(Files.exists(path));
+		Files.getLastModifiedTime(path);
+	}
+	
+	public void testCheckRead() throws URISyntaxException, IOException {
+		URI uri = new URI("hdfs://" + host + ":" + port + "/tmp/test_file");
+		Path path = Paths.get(uri);
+		if (Files.exists(path))
+			Files.delete(path);
+		assertFalse(Files.exists(path));
+		Files.createFile(path);
+		assertTrue(Files.exists(path));
 	}
 }
